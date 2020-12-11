782031946488c8eb25289e01772e0df013bfe44a
kukn1 <71047694+kukn1@users.noreply.github.com>
2020-12-10 19:09:33 -0500

    Update Q1-CProgramming.c

--- Q1-CProgramming.c
+++ Q1-CProgramming.c
@@ -7,20 +7,19 @@ The question has four parts, answer all of them in this same file*/
 /*(a) [5 Marks] write structure Node that holds an integer value as well as a pointer to the next node*/
 //Answer:
 
-
-
-
-
-
+struct Node{
+    
+    int value;
+    struct Node *next;
+};
 
 /*(b) [5 Marks] Write structure SLL that represents the singly linked list and holds a pointer to the first node in the list.*/
 //Answer:
 
-
-
-
-
-
+struct SLL{
+  
+  struct Node *head;
+};
 
 
 /*(c) [10 Marks] write function searchList, that takes as input two parameters: the head of a linked list "head" and the integer value to search for "target" and 
@@ -29,7 +28,15 @@ returns:
 0, otherwise*/
 //Answer:
 
-
+int searchList (struct Node *head, int target){
+    
+    for (struct Node *temp = head; temp != NULL; temp = temp->next){
+        
+        if (temp->value == target) return 1;
+        
+    }
+    return 0;
+}
 
 
   
@@ -39,17 +46,27 @@ the correctness of the returned value from the function by comparing it against 
 
 
 int main(){
+
+    struct Node* test1;
+    test1 = (struct Node*)malloc(sizeof(struct Node));
+    test1->value = 1;
+    struct Node* test2;
+    test2 = (struct Node*)malloc(sizeof(struct Node));
+    test2->value = 2;
+    test1->next = test2;
+	struct Node* test3;
+    test3 = (struct Node*)malloc(sizeof(struct Node));
+    test3->value = 3;
+    test2->next = test3;
+    
+    
+	int check = searchList(test1,2);
+	if (check == 1) printf("correct\n");
+	else printf("wrong\n");
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	int check2 = searchList(test1,42069);
+	if (check2 == 0) printf("correct\n");
+	else printf("wrong\n");
 	
 	
 	return 0;
